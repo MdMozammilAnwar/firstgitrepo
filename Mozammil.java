ab56564ad24fd959170c680a537849aa431dcf76
Quadri <mozammilmd6@gmail.com>
2020-06-10 12:05:05 -1200

    Mozammil.java file first commit, initial file no method

--- Mozammil.java
+++ Mozammil.java
@@ -1,10 +1,4 @@
 class Mozammil{
 public static void main(String[] args){
-System.out.println("Md Mozammil Anwar");
-message();
-}
-public static message()
-{
-	System.out.println("Good evening");
-}
+System.out.println("Hi, welcome to Md Mozammil Anwar Quadri");
 }
