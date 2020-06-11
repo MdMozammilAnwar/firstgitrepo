08544251ce585babfee170b38494306a8dc0d221
Quadri <mozammilmd6@gmail.com>
2020-06-10 12:11:26 -1200

    Mozammil.java file has added one message method, its just for message

--- Mozammil.java
+++ Mozammil.java
@@ -1,4 +1,10 @@
 class Mozammil{
 public static void main(String[] args){
 System.out.println("Hi, welcome to Md Mozammil Anwar Quadri");
+message();
+}
+public static void message()
+{
+	System.out.println("Good Morning");
+}
 }
